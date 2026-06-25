f936c3d07f065bbddf3589a9a463fa8f1481895e
Andrew Rissler <andrewhrissler@gmail.com>
2026-06-24 23:58:06 -0400

    Done for the Night
    This version has a very basic working bash script that creates a log
    file and thats it. Ive been at this for hours and am exhausted, gonna
    get dinner and then hmwk. Tmrw is figuring out how to get a prettier
    icon and making clicking on the icon work.

--- src/usr/local/emhttp/plugins/PlayniteNASyncUnraidPlugin/scripts/sync.sh
+++ src/usr/local/emhttp/plugins/PlayniteNASyncUnraidPlugin/scripts/sync.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+export PATH=/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+LOG="/usr/local/emhttp/plugins/PlayniteNASyncUnraidPlugin/sync.log"
 
-# Test file write
-echo "Script executed at $(date)" >> /usr/local/emhttp/plugins/PlayniteNASyncUnraidPlugin/sync.log
+{
+    echo "--- START $(date) ---"
+    # Try to list the file to prove the user has access
+    whoami
+    ls -l /usr/local/emhttp/plugins/PlayniteNASyncUnraidPlugin/scripts/sync.sh
+    echo "Running sync..."
+    # Your sync command here
+    echo "--- END $(date) ---"
+} > "$LOG" 2>&1
